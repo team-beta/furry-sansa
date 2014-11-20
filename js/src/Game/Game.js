38ec4d0be0956c37c046121bf703ea8cbc3be2cd
Pieter Kokx <pieter@kokx.nl>
2014-11-20 16:43:16 +0100

    Added a very simple platform.

--- js/src/Game/Game.js
+++ js/src/Game/Game.js
@@ -5,6 +5,9 @@ define([], function () {
         preload: function() {
         },
         create: function() {
+            for (var i = 0; i < 10; i++) {
+                this.add.sprite(i * 32, 32, 'grass_block');
+            }
         }
     };
 });
--- js/src/Game/Preload.js
+++ js/src/Game/Preload.js
@@ -10,6 +10,7 @@ define([], function () {
             this.preloadBar = this.add.sprite(this.camera.width/2 - (365/2), this.camera.height/2 - (49/2), 'preload_full');
             this.load.setPreloadSprite(this.preloadBar);
 
+            this.load.image('grass_block', 'assets/grass_block.png');
         },
         create: function() {
             this.state.start('Game');
