36f384a1dffc229902530a82166cc5d1e30c381a
Pieter Kokx <pieter@kokx.nl>
2014-11-24 11:50:59 +0100

    Added ability to position text boxes.

--- js/src/Game/Game.js
+++ js/src/Game/Game.js
@@ -6,6 +6,20 @@ define([], function () {
         },
         create: function() {
             this.add.tileSprite(64, 2*64, 10*64, 64, 'grass_block');
+
+            var spr = this.add.sprite(256, 256, 'grass_block');
+            spr.inputEnabled = true;
+            spr.input.useHandCursor = true;
+            spr.events.onInputDown.add(function(ev) {
+                console.log("Clicked!");
+                console.log(this);
+                console.log(ev);
+                pos = ev.position;
+                document.getElementById('main').innerHTML += '<div style="position: absolute; margin-top: ' + pos.y + 'px; margin-left: ' + pos.x + 'px; z-index: 10;"><textarea></textarea></div>';
+            }, this);
+        },
+        update: function() {
+            console.log('update');
         }
     };
 });
