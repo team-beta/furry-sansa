f0692fd55071ff82c292c6acb9051b77de1a2f44
Pieter Kokx <pieter@kokx.nl>
2014-11-24 12:53:59 +0100

    Added basic terminal.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 requirejs.config({
-    baseUrl: 'js/src',
-    paths: {
-        Phaser: '../vendor/phaser.min'
+    "baseUrl": 'js/src',
+    "paths": {
+        "Phaser": '../vendor/phaser.min',
+        "jquery": '../vendor/jquery-2.1.1.min',
+        "jquery-terminal": '../vendor/jquery.terminal-0.8.8.min'
+    },
+    "shim": {
+        "jquery-terminal": [ "jquery" ]
     }
 });
 
--- js/src/Game/Game.js
+++ js/src/Game/Game.js
@@ -1,4 +1,4 @@
-define([], function () {
+define(['jquery', 'jquery-terminal'], function ($) {
     return {
         init: function() {
         },
@@ -15,11 +15,25 @@ define([], function () {
                 console.log(this);
                 console.log(ev);
                 pos = ev.position;
-                document.getElementById('main').innerHTML += '<div style="position: absolute; margin-top: ' + pos.y + 'px; margin-left: ' + pos.x + 'px; z-index: 10;"><textarea></textarea></div>';
+                document.getElementById('main').innerHTML += '<div style="position: absolute; margin-top: ' + pos.y + 'px; margin-left: ' + pos.x + 'px; z-index: 10; background: orange;" id="term"></div>';
+                $('#term').terminal(function(command, term) {
+                    if (command !== '') {
+                        try {
+                            var result = window.eval(command);
+                            if (result !== undefined) {
+                                term.echo(new String(result));
+                            }
+                        } catch(e) {
+                            term.error(new String(e));
+                        }
+                    } else {
+                        term.echo('');
+                    }
+                }, {
+                    greetings: 'Javascript Interpreter',
+                    name: 'js_demo',
+                    height: 200,
+                    prompt: 'js> '});
             }, this);
-        },
-        update: function() {
-            console.log('update');
-        }
-    };
+        }    };
 });
