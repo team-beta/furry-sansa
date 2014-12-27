e89001f49c3badd1f7c22d6e5406d49c4c3e3507
Kevin Cleijne <kevin.cleijne@live.nl>
2014-12-28 00:31:04 +0100

    consistency check for the dancing robots
    no longer shall they be named randomly.
    
    also initialized them in level 1.
    DANCE MY MINIONS

--- js/src/Levels/lvl1/layer-1.js
+++ js/src/Levels/lvl1/layer-1.js
@@ -11,9 +11,11 @@ define([], function () {
         var width = this.game.world.width;
 
         // Add sprites
-      this.dancer1 =  this.game.add.sprite(17*tile, height - 18*tile, 'dancer');
-      this.dancer2 =  this.game.add.sprite(19*tile, height - 18*tile, 'dancer');
-      this.dancer3 =  this.game.add.sprite(21*tile, height - 18*tile, 'dancer');
+      this.dancer1 =  this.game.add.sprite(17*tile, height - 18*tile, 'dancer_blue');
+      this.dancer2 =  this.game.add.sprite(19*tile, height - 18*tile, 'dancer_red');
+      this.dancer3 =  this.game.add.sprite(21*tile, height - 18*tile, 'dancer_green');
+      this.dancer4 =  this.game.add.sprite(23*tile, height - 18*tile, 'dancer_orange');
+      
         this.game.add.sprite(0, height - 20*tile, 'crane');
         this.game.add.sprite(6*tile, height - 5*tile, 'tree_bg');
         this.game.add.sprite(8*tile, height - 20*tile, 'tree_bg');
@@ -33,6 +35,10 @@ define([], function () {
         this.dancer3.frame = 2;
         this.dancer3.animations.add('dance', [0,1,2,3,4,3,2,1], 10, true);
         this.dancer3.animations.play('dance');
+
+        this.dancer4.frame = 2;
+        this.dancer4.animations.add('dance', [0,1,2,3,4,3,2,1], 10, true);
+        this.dancer4.animations.play('dance');
 
     }
 
