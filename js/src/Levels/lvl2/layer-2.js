399e81552103d3c3b21c3e130a5679ee10ece1e7
TvS7 <t.v.s7_@hotmail.com>
2015-01-08 16:21:57 +0100

    Level 2 some more

--- js/src/Levels/lvl2/layer-2.js
+++ js/src/Levels/lvl2/layer-2.js
@@ -18,8 +18,8 @@ define(["Game/Block"], function (Block) {
         this.API.createPlatform(0,26*tile, width, 3*tile, this.main.fullMetalBlock);
 
         // Create weird wall on the left
-        this.API.createPlatform(3*tile, 6*tile, tile, 13*tile, this.main.fullMetalBlock);
-        this.API.createPlatform(4*tile, 7*tile, tile, 11*tile, this.main.fullMetalBlock);
+        this.API.createPlatform(3*tile, 8*tile, tile, 13*tile, this.main.fullMetalBlock);
+        this.API.createPlatform(4*tile, 8*tile, tile, 11*tile, this.main.fullMetalBlock);
         this.API.createPlatform(5*tile, 8*tile, tile, 10*tile, this.main.fullMetalBlock);
         this.API.createPlatform(6*tile, 9*tile, tile, 8*tile, this.main.fullMetalBlock);
         this.API.createPlatform(7*tile, 10*tile, tile, 7*tile, this.main.fullMetalBlock);
@@ -36,14 +36,27 @@ define(["Game/Block"], function (Block) {
         this.API.createPlatform(27*tile, 17*tile, 24*tile, tile, this.main.fullMetalBlock);
         this.API.createPlatform(28*tile, 10*tile, 23*tile, 7*tile, this.main.fullMetalBlock);
         this.API.createPlatform(27*tile, 8*tile, 24*tile, 2*tile, this.main.fullMetalBlock);
-        this.API.createPlatform(32*tile, 7*tile, 3*tile, tile, this.main.fullMetalBlock);
-        this.API.createPlatform(43*tile, 7*tile, 3*tile, tile, this.main.fullMetalBlock);
+        this.API.createPlatform(32*tile, 7*tile, 3*tile, tile, this.main.metalBlock);
+        this.API.createPlatform(43*tile, 7*tile, 3*tile, tile, this.main.metalBlock);
 
         // Create platforms
         this.API.createPlatform(9*tile, 6*tile, 3*tile, tile, this.main.metalBlock);
         this.API.createPlatform(15*tile, 6*tile, 3*tile, tile, this.main.metalBlock);
         this.API.createPlatform(22*tile, 7*tile, 2*tile, tile, this.main.metalBlock);
         this.API.createPlatform(21*tile, 15*tile, 3*tile, tile, this.main.metalBlock);
+
+        // Create walkways on blocks
+        this.API.createPlatform(0, 26*tile, 18*tile, tile, this.main.metalBlock);
+        this.API.createPlatform(18*tile, 22*tile, 5*tile, tile, this.main.metalBlock);
+        this.API.createPlatform(23*tile, 21*tile, tile, tile, this.main.metalBlock);
+        this.API.createPlatform(24*tile, 20*tile, tile, tile, this.main.metalBlock);
+        this.API.createPlatform(25*tile, 19*tile, tile, tile, this.main.metalBlock);
+        this.API.createPlatform(26*tile, 18*tile, tile, tile, this.main.metalBlock);
+        this.API.createPlatform(27*tile, 17*tile, tile, tile, this.main.metalBlock);
+        this.API.createPlatform(27*tile, 8*tile, 5*tile, tile, this.main.metalBlock);
+        this.API.createPlatform(35*tile, 8*tile, 8*tile, tile, this.main.metalBlock);
+        this.API.createPlatform(46*tile, 8*tile, 5*tile, tile, this.main.metalBlock);
+
 
 
         // Add interactable blocks
