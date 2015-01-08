f91e93e6749ce912d746fd84cf7a68c25bdf4323
TvS7 <t.v.s7_@hotmail.com>
2015-01-08 14:51:29 +0100

    Level 2 is the same as 1, but stuff will be added.

---
+++ js/src/Levels/lvl2/settings.js
@@ -0,0 +1,19 @@
+define([], function () {
+    var Settings = function(main) {
+        this.main = main;
+        this.snowflake = true;
+
+        // Set bounds
+        this.main.game.world.setBounds(0, 0, 1620, 920);
+
+        // Draw the background
+        this.main.game.add.tileSprite(0, 0, this.main.game.world.width, this.main.game.world.height, 'background');
+
+        // Background music.
+        this.music = this.main.game.add.audio('music_dododo');
+        this.music.play('', 0, 1, true, true);
+        this.main.bg_music = this.music;
+    }
+
+    return Settings;
+});
