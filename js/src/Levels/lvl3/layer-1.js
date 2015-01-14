e68943e2837bb3c33b4fc94a91a7038321919954
TvS7 <t.v.s7_@hotmail.com>
2015-01-14 13:49:18 +0100

    Level 3 made pretty

--- js/src/Levels/lvl3/layer-1.js
+++ js/src/Levels/lvl3/layer-1.js
@@ -11,10 +11,21 @@ define([], function () {
         var width = this.game.world.width;
 
         // Treegroups
-        this.game.add.sprite(33*tile, 22*tile, 'tree_group');
+        this.game.add.sprite(59*tile, 22*tile, 'tree_group');
 
+        // Add dancers
+        this.dancers = []
+        this.dancers[1] =  this.game.add.sprite(49*tile, 10*tile, 'dancer_blue');
+        this.dancers[2] =  this.game.add.sprite(51*tile, 10*tile, 'dancer_red');
+        this.dancers[3] =  this.game.add.sprite(53*tile, 10*tile, 'dancer_green');
+        this.dancers[4] =  this.game.add.sprite(55*tile, 10*tile, 'dancer_orange');
 
-
+        // Set animations for dancers
+        this.dancers.forEach(function(dancer){
+          dancer.frame = 2;
+          dancer.animations.add('dance', [0,1,2,3,4,3,2,1], 10, true);
+          dancer.animations.play('dance');
+        })
     }
 
     return Layer;
--- js/src/Levels/lvl4/layer-2.js
+++ js/src/Levels/lvl4/layer-2.js
@@ -21,8 +21,8 @@ define(["Game/Block"], function (Block) {
         this.API.createPlatform(9*tile, 27*tile, 6*tile, tile, this.main.grassBlock);
 
         // Create hills
-        this.API.createPlatform(15*tile, 12*tile, 12*tile, tile, this.main.grassBlock);
-        this.API.createPlatform(15*tile, 13*tile, 12*tile, 26*tile, this.main.dirtBlock);
+        this.API.createPlatform(15*tile, 8*tile, 12*tile, tile, this.main.grassBlock);
+        this.API.createPlatform(15*tile, 9*tile, 12*tile, 20*tile, this.main.dirtBlock);
 
         this.API.createPlatform(47*tile, 12*tile, 12*tile, tile, this.main.grassBlock);
         this.API.createPlatform(47*tile, 13*tile, 12*tile, 16*tile, this.main.dirtBlock);
@@ -35,7 +35,7 @@ define(["Game/Block"], function (Block) {
         this.API.createMattress(11*tile, 23*tile, function(){})
 
         //Create blocks
-        this.main.manager.create(17*tile, 3*tile, 2, 4, "block_1");
+        this.main.manager.create(17*tile, 3*tile, 2, 5, "block_1");
         this.main.manager.create(27*tile, 21*tile, 20, 2, "block_2");
 
         this.end = this.game.add.sprite(96*tile, 21*tile, 'end_level');
