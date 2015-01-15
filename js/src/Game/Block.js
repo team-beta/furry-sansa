1f9ebb1e084ae186a70ee47f38c6a5a17bc89516
Jeroen van Hoof <jmapvhooof@gmail.com>
2015-01-15 11:12:52 +0100

    Improved GUI for game.

--- js/src/Game/Block.js
+++ js/src/Game/Block.js
@@ -50,11 +50,20 @@ define(['jquery-terminal', 'Game/Task','jquery'], function (terminal, Task) {
                   $('.menu').html("").hide();
               }
 
-              if (this.main.levelNum >= 1) {
+              window.selectThing = function(name) {
+                  closeMenu();
+                  document.terminal.insert('world.blocks.' + name);
+                  $('#term .cmd').click();
+                  this.select();
+              }
+
+              $('.menu').append("<a href='javascript:void(0)' onclick='selectThing(\"" + name + "\")'>select</a>")
+
+              if (this.main.levelNum >= 2) {
               $('.menu').append("<a href='javascript:void(0)' onclick='choose(this, \"" + name + "\")'>" + ".setSolidity(" + !this.tileSprite.solid + ")" + "</a>")
               }
 
-              if (this.main.levelNum >= 2) {
+              if (this.main.levelNum >= 3) {
                   $('.menu').append("<a href='javascript:void(0)' onclick='choose(this, \"" + name + "\")'>" + ".moveLeft(" + 1 + ")" + "</a>")
                   $('.menu').append("<a href='javascript:void(0)' onclick='choose(this, \"" + name + "\")'>" + ".moveLeft(" + 5 + ")" + "</a>")
                   $('.menu').append("<a href='javascript:void(0)' onclick='choose(this, \"" + name + "\")'>" + ".moveLeft(" + 10 + ")" + "</a>")
